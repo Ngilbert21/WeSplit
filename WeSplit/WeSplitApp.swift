6e9d34cfb31400df0754ebe7595d59db81b2ae8e
Nicholas Gilbert <nicholasgilbert21@yahoo.com>
2023-01-10 12:46:44 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Nicholas Gilbert on 1/10/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
